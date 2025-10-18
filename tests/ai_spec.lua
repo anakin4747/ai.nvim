b58875d5a518c426cecb5676b8d90fd0e4678b72
Anakin Childerhose <anakin@childerhose.ca>
2025-10-18 14:30:58 -0400

    tests: add default test setup

---
+++ tests/ai_spec.lua
@@ -0,0 +1,13 @@
+require("plenary.busted")
+
+describe(":Ai command", function()
+
+    it("opens a new horizontal chat", function()
+        assert(true)
+    end)
+
+    it("opens a vertical chat with :vertical prefix", function()
+        assert(true)
+    end)
+
+end)
