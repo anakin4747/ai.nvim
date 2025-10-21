216aab7c40362f9d0b66b75e9c93ec49f5321bab
Anakin Childerhose <anakin@childerhose.ca>
2025-10-21 00:05:22 -0400

    tests: add failing test for passing a range to the buffer

--- tests/ai_spec.lua
+++ tests/ai_spec.lua
@@ -76,4 +76,20 @@ describe(":Ai command", function()
         end)
     end)
 
+    it("passes the selected range to the buffer", function()
+        vim.api.nvim_buf_set_lines(0, 0, -1, false, {
+            "De La Soul",
+            "Wu-Tang",
+            "C-Murder",
+            "A Tribe Called Quest",
+        })
+
+        vim.cmd('2,3Ai')
+
+        local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+        assert(vim.tbl_contains(lines, "Wu-Tang"))
+        assert(vim.tbl_contains(lines, "C-Murder"))
+    end)
+
 end)
