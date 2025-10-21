9a3ebadff93aac5bf3514c98dffe13beb63b2967
Anakin Childerhose <anakin@childerhose.ca>
2025-10-21 02:20:52 -0400

    drop no longer relavent tests
    Since the ! will dictate whether the buffer is reused, a test that
    ensures a buffer is created is no longer relavent.

--- tests/ai_spec.lua
+++ tests/ai_spec.lua
@@ -25,16 +25,6 @@ describe(":Ai command", function()
         assert(height_after < height_before, "Expected a horizontal split")
     end)
 
-    it("creates a new buffer", function()
-        local bufs_before = vim.api.nvim_list_bufs()
-
-        vim.cmd('Ai')
-
-        local bufs_after = vim.api.nvim_list_bufs()
-
-        assert.are.same(#bufs_before, #bufs_after - 1)
-    end)
-
     it("opens a new vertical window", function()
         local wins_before = vim.api.nvim_list_wins()
         local width_before = vim.api.nvim_win_get_width(0)
