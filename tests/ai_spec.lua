2728bdebec97537e2afb27df470fda72834975e6
Anakin Childerhose <anakin@childerhose.ca>
2025-11-14 01:30:32 -0500

    remove support for mocking home dir

--- tests/ai_spec.lua
+++ tests/ai_spec.lua
@@ -8,10 +8,10 @@ local function fixture_dir(test_name)
 end
 
 vim.g.ai_dir = default_mock_dir
--- support mocking home dir
-vim.g.ai_home_dir = os.getenv("HOME")
 
 vim.g.ai_localtime = 1763098419
+
+vim.g.ai_curl_stub_data = nil
 
 local function teardown()
     for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
