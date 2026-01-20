aa995f1b30495e5eae7174f0d2b19a25ad687d38
Anakin Childerhose <anakin@childerhose.ca>
2026-01-19 22:00:37 -0500

    add first version of test server

--- tests/test_server.lua
+++ tests/test_server.lua
@@ -2,10 +2,41 @@
 local pegasus = require('pegasus')
 
 local server = pegasus:new({
-  port='80',
-  location='root/'
+    port = '80',
+    location = 'root/'
 })
 
-server:start(function (request, response)
-  -- print "It's running..."
+-- Helper to read file content
+local function read_file(path)
+    local f = io.open(path, "r")
+    if not f then return nil end
+    local content = f:read("*all")
+    f:close()
+    return content
+end
+
+-- Resolve absolute path relative to project root
+local function get_endpoint_path(subpath)
+    local cwd = vim.loop.cwd()
+    return cwd .. "/endpoints/" .. subpath
+end
+
+server:start(function(request, response)
+    local path = request:path()
+    print("Request path: " .. path)
+
+    if path == '/copilot_internal/v2/token' then
+        response:statusCode(200)
+        response:write(read_file(get_endpoint_path('token/good.json')))
+    elseif path == '/models' then
+        response:statusCode(200)
+        response:write(read_file(get_endpoint_path('models/good.json')))
+    elseif path == '/chat/completions' then
+        response:statusCode(200)
+        response:write(read_file(get_endpoint_path('chats/good.json')))
+    else
+        response:statusCode(404)
+        response:write("Not Found")
+    end
 end)
+
